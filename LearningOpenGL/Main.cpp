ba4561506e0b86f807e3b1c0ff970ec7cd44f74e
Taylor Ellington <taylorellington101@gmail.com>
2017-10-04 07:51:14 -0700

    Fixed stbi_load

--- LearningOpenGL/Main.cpp
+++ LearningOpenGL/Main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include "Display.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_FAILURE_USERMSG
+#include "stb_image.h"
+
 int main(int argc, char** argv) {
 
 	Display display(640, 480, "Benny's shit tutorial");
 	display.MakeCurrent();
+	int x, y, n;
+	unsigned char * imageData = stbi_load("brick.jpg", &x, &y, &n, 4);
+
+	if (imageData == NULL) {
+		std::cerr << stbi_failure_reason() << std::endl;
+	}
+		
+
 
 	while (!display.ShouldClose()) {
 		display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
